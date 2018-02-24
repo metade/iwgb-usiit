49e908682680f2d35946ad610aa4f070ba19c875
Patrick Sinclair <metade@gmail.com>
2018-02-24 16:05:56 +0000

    Styling'

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,4 +1,12 @@
 class Issue < ApplicationRecord
   belongs_to :meeting
   has_many :votes
+
+  def vote_count
+    votes.where(status: %i[agree sympathise]).count
+  end
+
+  def vote_result
+    (votes.where(status: %i[agree]).count / vote_count.to_f) * 100
+  end
 end
