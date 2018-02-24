4acff633743bbfe828281155be9c5b80122591c2
Patrick Sinclair <metade@gmail.com>
2018-02-24 16:21:23 +0000

    Recast vote and footer

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,11 +1,16 @@
 class VotesController < ApplicationController
+  before_action :setup_parent_variables
+
   def create
-    @branch = Branch.find(params[:branch_id])
-    @meeting = @branch.meetings.find(params[:meeting_id])
-    @issue = @meeting.issues.find(params[:issue_id])
     @vote = @issue.votes.new(vote_params)
 
     redirect_to [@branch, @meeting] if @vote.save
+  end
+
+  def destroy
+    @vote = Vote.where(issue: @issue).find(params[:id])
+
+    redirect_to [@branch, @meeting] if @vote.destroy
   end
 
   private
@@ -19,5 +24,11 @@ class VotesController < ApplicationController
 
     session[:voted] = true
     session.id
+  end
+
+  def setup_parent_variables
+    @branch = Branch.find(params[:branch_id])
+    @meeting = @branch.meetings.find(params[:meeting_id])
+    @issue = @meeting.issues.find(params[:issue_id])
   end
 end
