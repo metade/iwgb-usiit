7efe61f2800544dec4ac8c22f02bae55634184f5
Patrick Sinclair <metade@gmail.com>
2018-02-24 17:54:50 +0000

    fixing result

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -3,10 +3,10 @@ class Issue < ApplicationRecord
   has_many :votes, dependent: :destroy
 
   def vote_count
-    votes.where(status: %i[agree sympathise]).count
+    votes.where(status: %i[agree disagree sympathise]).count
   end
 
-  def vote_result
-    (votes.where(status: %i[agree]).count / vote_count.to_f) * 100
+  def vote_result(status)
+    (votes.where(status: status).count / vote_count.to_f) * 100
   end
 end
